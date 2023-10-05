4ae90255163bfe900d2d9f8a6dcc3eeaf7682ffb
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-05 07:20:06 -0700

    6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of char: %d byte(s)\n
+			Size of int: %d byte(s)\n
+			Size of long int: %dbyte(s)\n
+			Size of long long int: %dbyte(s)\n
+			Size of float: %dbyte(s)\n",
+			sizeof(char),
+			sizeof(int),
+			sizeof(long int),
+			sizeof(long long int),
+			sizeof(float));
+}
