a614fd01ddb0622dcc19a6c10726e565c3b92f4f
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-05 11:46:34 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char *buffer = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char *buffer = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, buffer, 58);
 
