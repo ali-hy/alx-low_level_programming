6912efaab258701f9144b86d2261ff747059a982
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-10 09:07:29 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - print the alphabet in lower case letters
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet()
+{
+	char c;
+	
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar('a');
+	}
+	_putchar('\n');
+}
