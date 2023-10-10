b59812adfefbe59dafc78ce7feb7eeeff8ef093a
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-10 12:24:02 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @a: any int
+ * @b: any int
+ *
+ * Return: the sum of a and b
+ */
+int sum(int a, int b)
+{
+	return (a + b);
+}
