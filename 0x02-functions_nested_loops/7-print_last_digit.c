4ec8575132807ae79b13ace796193610449fb67e
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-10 11:42:45 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,10 +13,9 @@ int print_last_digit(int n)
 
 	if (last_digit < 0)
 	{
-		return (-last_digit);
+		last_digit = -last_digit
 	}
-	else
-	{
-		return (last_digit);
-	}
+	
+	_putchar('0' + last_digit);
+	return (last_digit);
 }
