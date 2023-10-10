3c5efc011a047d4f1f45207f77e26ef412b7178b
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-10 11:55:45 -0700

    8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_two_digits - prints ints as 2 digits
+ */
+void print_two_digits(int n)
+{
+	_putchar('0' + (n / 10));
+	_putchar('0' + (n % 10));
+}
+
+/**
+ * jack_bauer - print every minute from 00:00 till 23:59
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			print_two_digits(i);
+			print_two_digits(j);
+			_putchar('\n');
+		}
+	}
+}
