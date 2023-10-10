83710252dfc06a175f1329447e456d6272456917
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-10 10:53:11 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,9 +10,21 @@
 int print_sign(int n)
 {
 	if (n < 0)
+	{
+		_putchar('-');
+		_putchar('\n');
 		return (-1);
+	}	
 	else if (n < 0)
+	{
+		_putchar('+');
+		_putchar('\n');
 		return (1);
+	}
 	else
+	{
+		_putchar('0');
+		_putchar('0');
 		return (0);
+	}
 }
