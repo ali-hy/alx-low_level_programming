7743af056a596f5a0d847c1e375146413fcbe167
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-10 12:13:41 -0700

    main.h

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_number - prints int to take up 3 spaces
+ * @n: any valid int
+ */
+void print_number(int n)
+{
+	_putchar(' ');
+	if (n / 10 > 0)
+	{
+		_putchar('0' + (n / 10));
+	}
+	else
+	{
+		_putchar(' ');
+	}
+	_putchar('0' + n % 10);
+}
+
+
+/**
+ * times_table - print the 9s timetable
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 9; i++)
+	{
+		for (j = 0; j < 9; j++)
+		{
+			print_number(i * j);
+			_putchar(',');
+		}
+		_putchar('\n');
+	}
+}
