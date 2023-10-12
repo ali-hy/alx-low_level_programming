1c8123803ee98ed8affd449dbba750165a734406
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-12 10:05:25 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagonal: prints a diagonal line
+ * @size: size of diagonal line
+ */
+void print_diagonal(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < i - 1; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
