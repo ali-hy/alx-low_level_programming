4a6e8f68f281f3f505d6aa44311f87c0f96ee2f0
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-12 08:52:23 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - print triangle
+ * @size: size of triangle
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			if (size - j >= i)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
+}
---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**
+ * print_square - prints a square with dimensions n
+ * @size: side length of square
+ */
+void print_square(int size)
+{
+	
+}
