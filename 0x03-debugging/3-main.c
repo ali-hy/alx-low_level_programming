28ed8cdbc447e36c46557b09a5eae279fd79b1f9
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-12 05:53:45 -0700

    reset 3-print_remaining_days.c to original (for testing)

--- 0x03-debugging/3-main.c
+++ 0x03-debugging/3-main.c
@@ -14,14 +14,16 @@ int main(void)
 	int year;
 
 	month = 2;
-	day = 29;
-	year = 2000;
 
-	printf("Date: %02d/%02d/%04d\n", month, day, year);
+	for (year = 0; year <= 4000; year += 4)
+	{
+		day = 29;
+		printf("Date: %02d/%02d/%04d\n", month, day, year);
 
-	day = convert_day(month, day);
+		day = convert_day(month, day);
 
-	print_remaining_days(month, day, year);
-
+		print_remaining_days(month, day, year);
+		printf("\n");
+	}
 	return (0);
 }
