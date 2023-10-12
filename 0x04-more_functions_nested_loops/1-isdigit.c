094523c411623f967bd83620bb5db83609e5c307
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-12 10:59:31 -0700

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isdigit - check if character is digit
+ * @c: character
+ * Return: 1 if c is a digit, 0 if c is not a digit
+ */
+int _isdigit(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	return (0);
+}
