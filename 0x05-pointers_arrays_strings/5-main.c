43b647433741e348950fe7983b1b49d5e576131c
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-16 03:55:50 -0700

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-main.c
+++ 0x05-pointers_arrays_strings/5-main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
-#include "main.h"
+
+void rev_string(char *);
 
 /**
- * main - entry point
+ * main - check the code for Holberton School students.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	char str[] = "Holberton!";
-	printf("%s\n", str);
-	rev_string(str);
-	printf("%s\n", str);
+	char s[11] = "Holberton!";
 
+	printf("%s\n", s);
+	rev_string(s);
+	printf("%s\n", s);
 	return (0);
 }
