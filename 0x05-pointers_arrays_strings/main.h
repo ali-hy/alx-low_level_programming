8076991a7463ef5821caa0f611650aad0e6751f7
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-15 23:59:40 -0700

    fill main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,5 +2,14 @@
 #define MAIN_H
 
 void reset_to_void(int*);
+void swap_int(int*, int*);
+int _strlen(char*);
+void _puts(char*);
+void print_rev(char*);
+void rev_string(char*);
+void puts2(char*);
+void puts_half(char*);
+void print_array(int*, int);
+char* _strcpy(char*, char*);
 
 #endif /* MAIN_H */
