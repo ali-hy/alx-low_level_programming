50cd55c8b4de267e2b4a2adb099b332c383f5064
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-16 05:57:27 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -5,15 +5,18 @@
  * _strcpy - cpy a string from source to destination
  * @dest: the destination string
  * @src: the source string
+ * Return: returns destination string
  */
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-	
+
 	for (i = 0; src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
 
-	return dest;
+	dest[i] = '\0';
+
+	return (dest);
 }
