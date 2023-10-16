3f6bdd96088c9345b740b52c605aecc27a362c45
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-16 03:59:47 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts2 - print every other character of a string
+ * @s: a string
+ */
+void puts2(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (i%2 == 0)
+			_putchar(s[i]);
+	}
+}
