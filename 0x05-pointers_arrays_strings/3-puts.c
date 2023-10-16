3ff363c7182b0b1438cafd0bfa8931650c5848ed
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-16 00:36:58 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - put string in stdout
+ * @s: string to print
+ */
+void _puts(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+	}
+}
