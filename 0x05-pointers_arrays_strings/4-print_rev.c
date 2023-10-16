7b6bb4fbb0eb40df95caf7d62df999ac86a857d1
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-16 00:50:40 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: string to print
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+
+	while (len--)
+	{
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}
