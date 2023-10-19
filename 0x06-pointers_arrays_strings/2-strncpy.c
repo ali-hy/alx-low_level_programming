d4d6e5be70c809054b2d684ce8d3a5dae0031b5b
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-19 11:01:56 -0700

    fix 2-strncpy.c null termination

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -17,8 +17,11 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = src[i];
 	}
 
-	if (src[i] == '\0')
-		dest[i] = src[i];
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
 
 	return (dest);
 }
