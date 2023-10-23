a1b029883739b2c39983acef9eaebadeae84d425
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-23 19:53:58 +0300

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strspn - Returns the number of bytes in the initial segment of s which
+ * consist only of bytes from accept
+ * @s: string
+ * @accept: letters expected in prefix
+ * Return: the number of bytes in the initial segment of s which consist
+ * only of bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, is_prefix;
+
+	for (i = 0; is_prefix && s[i] != '\0'; i++)
+	{
+		is_prefix = 0;
+
+		for (j = 0; accept[j] != '\0'; i++)
+		{
+			if (accept[j] == s[i])
+			{
+				is_prefix = 1;
+				break;
+			}
+		}
+	}
+}
