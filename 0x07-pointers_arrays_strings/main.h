da4c4dd5e3a3f74065e1a886f106e199fb268e15
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-23 21:31:11 +0300

    fix 8-print_diagsums.c: prototype in main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,7 +9,7 @@ unsigned int _strspn(char*, char*);
 char *_strpbrk(char*, char*);
 char *_strstr(char*, char*);
 void print_chessboard(char (*a)[8]);
-void print_diagsums(int, int);
+void print_diagsums(int*, int);
 void set_string(char**, char*);
 
 #endif /* MAIN_H */
