df83d4132f977a0bcedea4924a1636412da28c06
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-25 16:16:26 +0300

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts_recursion - write string to output and add a newline
+ * @s: string to print
+ */
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_putchar(s[0]);
+	_puts_recursion(s + 1);
+}
