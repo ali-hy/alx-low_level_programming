f7750922b335edf115d99915cf3357bd643a8d90
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-25 18:22:51 +0300

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,13 +5,13 @@
  * divisible_by - check if n is divisible by any numbers from x to n
  * @n: dividend
  * @x: divider
- * Return 1 if divisible, reutrn 0 if not divisible
+ * Return: 1 if divisible, reutrn 0 if not divisible
  */
 int divisible_by(int n, int x)
 {
 	if (x * x > n)
 		return (0);
-	
+
 	if (n % x == 0)
 		return (1);
 
@@ -21,11 +21,11 @@ int divisible_by(int n, int x)
 /**
  * is_prime_number - check if n is a prime number
  * @n: number to check
- * return 1 if prime, return 0 if not
+ * Return: 1 if prime, return 0 if not
  */
 int is_prime_number(int n)
 {
 	if (n <= 1)
-		return 0;
+		return (0);
 	return (divisible_by(n, 2) == 0 ? 1 : 0);
 }
