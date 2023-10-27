465ce167bf2def3e4ccedbebfa44ae3707bf4b8d
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-27 23:12:42 +0200

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
