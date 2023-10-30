cafcb308fdda0fbfeb3bb28f0e3cbec4ef55c3b8
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-30 16:43:41 +0200

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strdup - copy an existing string into a dynamic array
+ * @str: string
+ * Return: pointer to the new string
+ */
+char *_strdup(char *str)
+{
+	int i, size = 0;
+	char *res;
+
+	if (str == NULL)
+		return (NULL);
+
+
+	while(str[size] != '\0')
+	{
+		size++;
+	}
+
+	res = malloc(sizeof(char) * size);
+	if (res == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i <= size; i++)
+	{
+		res[i] = str[i];
+	}
+
+	return (res);
+}
