3d1b6b434134a4d6ac24fb7b74bfbe37e860800e
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-30 16:27:26 +0200

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,16 +3,19 @@
 
 /**
  * crate_array - creates a char array and initializes it with a char
+ * @size: size of the array
+ * @c: character to fill the array with
+ * Return: pointer to array
  */
 char *create_array(unsigned int size, char c)
 {
 	int i;
 	char *res;
 
-	if (size = 0)
+	if (size == 0)
 		return NULL;
 
-	res = malloc(sizeof(char) * 8);
+	res = malloc(sizeof(char) * size);
 	if (res == NULL)
 	{
 		return (1);
