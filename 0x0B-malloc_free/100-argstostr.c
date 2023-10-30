022a464be073c9a84dcf5cf9ec6578e5d93b4521
ali-hy <ali.haitham.ymy@gmail.com>
2023-10-30 23:26:11 +0200

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - get string length
+ * @s: string
+ * Return: length of s
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (s == NULL)
+		return (-1);
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+
+/**
+ * argstostr - concatinate program arguments into a string
+ * @ac: argument count
+ * @av: argument array
+ * Return: pointer to concatinated string
+ */
+char *argstostr(int ac, char **av)
+{
+	int total_size = 0, i, j, k = 0;
+	char **res;
+
+	for (i = 0; i < ac; i++)
+	{
+		total_size += _strlen(av[i]);
+	}
+
+	res = malloc(sizeof(char) * (total_size + ac - 1));
+	if (res == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, k++)
+		{
+			 res[k] = av[i][j];
+		}
+		res[k] = '\n';
+		k++;
+	}
+
+	return (res);
+}
