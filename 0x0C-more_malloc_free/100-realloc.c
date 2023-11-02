fa20cf90d8b15682634a85e8009dd1ce8e4ed5d0
ali-hy <ali.haitham.ymy@gmail.com>
2023-11-02 20:12:49 +0200

    100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -28,7 +28,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (res == NULL)
 		return (NULL);
 
-	for (i = 0; i < old_size; i++)
+	for (i = 0; i < smaller_size; i++)
 		res[i] = ptr_c[i];
 
 	if (ptr != NULL)
