2453854c9cfe9fe10ef08219e8f1e6596ed9e343
ali-hy <ali.haitham.ymy@gmail.com>
2023-11-06 19:29:06 +0200

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * free_dog - free a dog from it's chains, and its jail #end_slavery
+ * @d: imprisoned dog, chained to the wall
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
