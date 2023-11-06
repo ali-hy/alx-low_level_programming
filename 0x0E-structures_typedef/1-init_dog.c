9d03f1a625d77572de8e0b5ef56619358084a5e2
ali-hy <ali.haitham.ymy@gmail.com>
2023-11-06 16:13:08 +0200

    1-init_dog.c

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - inits a dog struct
+ * @d: dog struct to init
+ * @name: name prop
+ * @age: age prop
+ * @owner: owner prop
+ */
+void init_dog(sturct dog *d, char *name, float age, char *owner)
+{
+	dog->name = name;
+	dog->age = age;
+	owner->owner = owner;
+}
