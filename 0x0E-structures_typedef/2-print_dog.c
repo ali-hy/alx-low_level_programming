aeddcd1f0021cdab1a500a9620fdfe598a6a9543
ali-hy <ali.haitham.ymy@gmail.com>
2023-11-06 16:53:26 +0200

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * nin - nil if null, returns "(nil)" if the string is NULL, or the string if
+ * it's not null
+ * @s: string
+ * Return: (nil) if s is NULL, or s if it's not NULL
+ */
+char *nin(char *s)
+{
+	if (s == NULL)
+		return ("(nil)");
+
+	return (s);
+}
+
+/**
+ * print_dog - prints dog struct
+ * @d: dog struct
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	
+	printf("Name: %s\nAge: %.6f\nOwner: %s\n",
+			nin(d->name), d->age, nin(d->owner));
+}
