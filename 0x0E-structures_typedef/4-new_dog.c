22a27e115b1e46f929d56b9a40ca914cda0d3705
ali-hy <ali.haitham.ymy@gmail.com>
2023-11-06 18:48:41 +0200

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -51,5 +51,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d->age = age;
 	d->owner = strcp(owner);
 
+	if (d->name == NULL)
+		return (NULL);
+	
+	if (d->owner == NULL)
+		return (NULL);
 	return (d);
 }
