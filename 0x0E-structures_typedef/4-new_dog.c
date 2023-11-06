a3f2e4de8a29f4bd14ef0e82bcdcdab256c974df
ali-hy <ali.haitham.ymy@gmail.com>
2023-11-06 17:45:08 +0200

    4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * strcp - copy string
+ * @str: string to copy
+ * Return: copy of string
+ */
+char *strcp(char *str)
+{
+	int i = 0, size;
+	char *res;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
+	size = i + 1;
+	res = malloc(sizeof(char) * size);
+	if (res == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		res[i] = str[i];
+	}
+
+	return (res);
+}
+
+/**
+ * new_dog - creates a new dog
+ * @name: name prop
+ * @age: age prop
+ * @owner: owner prop
+ * Return: pointer to new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+	d = malloc(sizeof(dog_t));
+
+	d->name = strcp(name);
+	d->age = age;
+	d->owner = strcp(owner);
+
+	return (d);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdlib.h>
+
 /**
  * struct dog - pet dog
  * @name: dog's name
