2020e140ea5682533e49759002783a16eadae5c4
ali-hy <ali.haitham.ymy@gmail.com>
2023-11-08 21:26:22 +0200

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -6,10 +6,16 @@
  * @array: the array to do weird things on
  * @size: array size
  * @cmp: function that tells you what to return
+ * Return: something
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
+
+	if (array == NULL)
+		return (-1);
+	if (cmp == NULL)
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
