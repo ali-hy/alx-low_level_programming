049ea4595ffe18145ebaa7d2836018033117bb36
ali-hy <ali.haitham.ymy@gmail.com>
2023-11-08 20:49:09 +0200

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_name - print a name
+ * @name: name to print
+ * @f: function to use for printing the name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL)
+		return;
+
+	f(name);
+}
