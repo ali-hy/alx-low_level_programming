7bed686f2eeff3ea4a585cf0a49ea7e3d78d5cdd
ali-hy <ali.haitham.ymy@gmail.com>
2023-11-09 16:59:32 +0200

    1-print_numbers.c

--- 0x10-variadic_functions/1-main.c
+++ 0x10-variadic_functions/1-main.c
@@ -8,5 +8,8 @@
 int main(void)
 {
     print_numbers(", ", 4, 0, 98, -1024, 402);
+	print_numbers(" - ", 4, 1, 2, 3, 4);
+	print_numbers("", 3, 0, 1, 2);
+	print_numbers(NULL, 3, 1, 2, 3);
     return (0);
 }
