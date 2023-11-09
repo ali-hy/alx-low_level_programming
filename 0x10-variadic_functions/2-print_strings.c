50c3b06db8d95ee5f2508992c2a37af252f1f516
ali-hy <ali.haitham.ymy@gmail.com>
2023-11-09 19:46:22 +0200

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -19,7 +19,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	}
 
 	va_start(strs, n);
-	for (i = 0; i < n - 1, i++)
+	for (i = 0; i < n - 1; i++)
 	{
 		str = va_arg(strs, char *);
 		str = str == NULL ? "(nil)" : str;
