5b6c4be720eb911de0335954c74a9c3fd3292384
ali-hy <ali.haitham.ymy@gmail.com>
2023-11-17 01:31:53 +0200

    betty 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/**
+ * premain - function that runs before main
+ */
 void __attribute__((constructor)) premain()
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
