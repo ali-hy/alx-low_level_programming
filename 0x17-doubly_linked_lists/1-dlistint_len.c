295cb4a06d35de5505a9603c85cdc54244b79a0a
ali-hy <ali.haitham.ymy@gmail.com>
2024-01-11 22:14:53 +0200

    1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - get length of doubly linked list
+ * @h: head of doubly linked list
+ * Return: length of doubly linked list
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t l;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		l++;
+	}
+
+	return (l);
+}
