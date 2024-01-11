b15c5eaa93c74422130121aa9c8cbb66f59fb87b
ali-hy <ali.haitham.ymy@gmail.com>
2024-01-12 01:49:02 +0200

    5-get_dnodeint.c

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - ...
+ * @head: ...
+ * @index: ...
+ * Return: ...
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	while (head != NULL)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++;
+	}
+
+	return (NULL);
+}
