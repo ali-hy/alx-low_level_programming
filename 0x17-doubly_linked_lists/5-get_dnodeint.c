e146dec1c48bb0f229d63f90b1a584caf62a4b8c
ali-hy <ali.haitham.ymy@gmail.com>
2024-01-12 02:13:56 +0200

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -20,3 +20,5 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 
 	return (NULL);
 }
+
+/* sodium iodide + Radium + Hydrogen */
---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,44 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - ...
+ * @h: ...
+ * @idx: ...
+ * @n: ...
+ * Return: ...
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node = NULL, *prev = *h, *next = NULL;
+	unsigned int i = 1;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+
+	while (prev != NULL)
+	{
+		if (i == idx)
+			break;
+		prev = prev->next;
+		i++;
+	}
+
+	if (prev == NULL)
+		return (NULL);
+
+	next = prev->next;
+
+	prev->next = new_node;
+	new_node->prev = prev;
+
+	next->prev = new_node;
+	new_node->next = next;
+
+	return (new_node);
+}
