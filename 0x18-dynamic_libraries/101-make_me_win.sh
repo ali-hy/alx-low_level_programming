0cbb5d9d85eca062e73526d84d4b8e35d0dc0f64
ali-hy <ali.haitham.ymy@gmail.com>
2024-01-19 19:34:38 +0200

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget --retry-connrefused --waitretry=1 --read-timeout=20 --timeout=15 -t 0 --continue -O /tmp/myrand.so https://github.com/ali-hy/alx-low_level_programming/raw/main/0x18-dynamic_libraries/lib/myrand.so
-export LD_PRELOAD='/tmp/myrand.so'
+wget -O /tmp/win.so https://github.com/Zevrov/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/win.so
+export LD_PRELOAD=/tmp/win.so
