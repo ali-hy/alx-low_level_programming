63652c069ecd111887fcc7d9d12119c16ee89820
ali-hy <ali.haitham.ymy@gmail.com>
2024-03-21 11:13:21 +0200

    0-MAKEFILE

---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - entry point to program
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf("hello world\n");
+
+	return (0);
+}
---
+++ 0x1C-makefiles/school.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * school - entry point to program
+ * Return: 0 on success
+ */
+int school(void)
+{
+	printf("hello school\n");
+
+	return (0);
+}
